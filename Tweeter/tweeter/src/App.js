c795e13b0023ca1cf003ed986b441dd56a66a2b1
Quincey Hollman <qhollman@gmail.com>
2020-03-10 22:27:48 -0700

    submission Eventonica-Part-7

--- Tweeter/tweeter/src/App.js
+++ Tweeter/tweeter/src/App.js
@@ -1,30 +1,53 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 
-handleMessage = event => {
-  this.ListeningStateChangedEvent({ message: event.target.value });
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: "" };
+  }
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleMessageChange = event => {
+    this.setState({ message: event.target.value });
+  };
+
+  render() {
+    const message = this.state.message,
+      maxCharacterCount = 100,
+      currentCharacterCount = message.length;
+
+     
+
+    return (
+      <div>
+        <h1>Tweeter</h1>
+
+        <label for='entryform'>Text Form</label>
+        <form name='entryform' id='entry-form'>
+          <select name='usernames' id='usernames'>
+            <option value='_user1'>User1</option>
+            <option value='_user2'>User2</option>
+            <option value='_user3'>User3</option>
+            <option value='_user4'>User4</option>
+            <option value='_user5'>User5</option>
+          </select>{" "}
+          <br />
+          <textarea
+            value={message}
+            onChange={this.handleMessageChange}
+          ></textarea>
+          <br />
+          <span id = "character" > 
+          Character Count: {overCharacterLimit} 
+          {currentCharacterCount <= maxCharacters && currentCharacterCount >= 100 
+          : <span style={{color: "red"}}> Over Character Limit!</span>
+          }
+             </span>
+          <input type='submit' value='Submit'></input>
+          <br />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
